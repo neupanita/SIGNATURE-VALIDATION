82672a431ced8a5b50db53c8bedbd1a6779c0952
neupanita <neup.anita@gmail.com>
2018-05-20 11:53:50 -0500

    Add files via upload

---
+++ SignatureValidationApplication/SignatureValidationApplication/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace SignatureValidationApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbresult = new System.Windows.Forms.TextBox();
+            this.btBrowseResult = new System.Windows.Forms.Button();
+            this.dgCSVData = new System.Windows.Forms.DataGridView();
+            this.dgRepository = new System.Windows.Forms.DataGridView();
+            this.btnRepository = new System.Windows.Forms.Button();
+            this.tbRepository = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtExport = new System.Windows.Forms.Button();
+            this.btUpload = new System.Windows.Forms.Button();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.txtClear = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCSVData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgRepository)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File path:";
+            // 
+            // tbresult
+            // 
+            this.tbresult.Location = new System.Drawing.Point(71, 18);
+            this.tbresult.Name = "tbresult";
+            this.tbresult.Size = new System.Drawing.Size(269, 20);
+            this.tbresult.TabIndex = 1;
+            // 
+            // btBrowseResult
+            // 
+            this.btBrowseResult.Location = new System.Drawing.Point(346, 18);
+            this.btBrowseResult.Name = "btBrowseResult";
+            this.btBrowseResult.Size = new System.Drawing.Size(101, 23);
+            this.btBrowseResult.TabIndex = 2;
+            this.btBrowseResult.Text = "Browse Result";
+            this.btBrowseResult.UseVisualStyleBackColor = true;
+            this.btBrowseResult.Click += new System.EventHandler(this.btBrowseResult_Click);
+            // 
+            // dgCSVData
+            // 
+            this.dgCSVData.AllowUserToAddRows = false;
+            this.dgCSVData.AllowUserToDeleteRows = false;
+            this.dgCSVData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCSVData.Location = new System.Drawing.Point(12, 61);
+            this.dgCSVData.Name = "dgCSVData";
+            this.dgCSVData.ReadOnly = true;
+            this.dgCSVData.Size = new System.Drawing.Size(572, 216);
+            this.dgCSVData.TabIndex = 4;
+            // 
+            // dgRepository
+            // 
+            this.dgRepository.AllowUserToAddRows = false;
+            this.dgRepository.AllowUserToDeleteRows = false;
+            this.dgRepository.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgRepository.Location = new System.Drawing.Point(12, 338);
+            this.dgRepository.Name = "dgRepository";
+            this.dgRepository.ReadOnly = true;
+            this.dgRepository.Size = new System.Drawing.Size(572, 219);
+            this.dgRepository.TabIndex = 4;
+            // 
+            // btnRepository
+            // 
+            this.btnRepository.Location = new System.Drawing.Point(346, 299);
+            this.btnRepository.Name = "btnRepository";
+            this.btnRepository.Size = new System.Drawing.Size(111, 23);
+            this.btnRepository.TabIndex = 7;
+            this.btnRepository.Text = "Browse Repository";
+            this.btnRepository.UseVisualStyleBackColor = true;
+            this.btnRepository.Click += new System.EventHandler(this.btnRepository_Click);
+            // 
+            // tbRepository
+            // 
+            this.tbRepository.Location = new System.Drawing.Point(71, 299);
+            this.tbRepository.Name = "tbRepository";
+            this.tbRepository.Size = new System.Drawing.Size(269, 20);
+            this.tbRepository.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File path:";
+            // 
+            // txtExport
+            // 
+            this.txtExport.Location = new System.Drawing.Point(494, 580);
+            this.txtExport.Name = "txtExport";
+            this.txtExport.Size = new System.Drawing.Size(75, 23);
+            this.txtExport.TabIndex = 8;
+            this.txtExport.Text = "Export";
+            this.txtExport.UseVisualStyleBackColor = true;
+            this.txtExport.Click += new System.EventHandler(this.txtExport_Click);
+            // 
+            // btUpload
+            // 
+            this.btUpload.Location = new System.Drawing.Point(468, 20);
+            this.btUpload.Name = "btUpload";
+            this.btUpload.Size = new System.Drawing.Size(101, 23);
+            this.btUpload.TabIndex = 3;
+            this.btUpload.Text = "Upload CSV";
+            this.btUpload.UseVisualStyleBackColor = true;
+            this.btUpload.Click += new System.EventHandler(this.btUpload_Click);
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(494, 301);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(75, 23);
+            this.btnValidate.TabIndex = 8;
+            this.btnValidate.Text = "Validate";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // txtClear
+            // 
+            this.txtClear.Location = new System.Drawing.Point(401, 580);
+            this.txtClear.Name = "txtClear";
+            this.txtClear.Size = new System.Drawing.Size(75, 23);
+            this.txtClear.TabIndex = 9;
+            this.txtClear.Text = "Clear";
+            this.txtClear.UseVisualStyleBackColor = true;
+            this.txtClear.Click += new System.EventHandler(this.txtClear_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Coral;
+            this.label3.Location = new System.Drawing.Point(12, 583);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 618);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtClear);
+            this.Controls.Add(this.txtExport);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.btnRepository);
+            this.Controls.Add(this.tbRepository);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgRepository);
+            this.Controls.Add(this.dgCSVData);
+            this.Controls.Add(this.btUpload);
+            this.Controls.Add(this.btBrowseResult);
+            this.Controls.Add(this.tbresult);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CSVImporter";
+            ((System.ComponentModel.ISupportInitialize)(this.dgCSVData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgRepository)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbresult;
+        private System.Windows.Forms.Button btBrowseResult;
+        private System.Windows.Forms.DataGridView dgCSVData;
+        private System.Windows.Forms.DataGridView dgRepository;
+        private System.Windows.Forms.Button btnRepository;
+        private System.Windows.Forms.TextBox tbRepository;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button txtExport;
+        private System.Windows.Forms.Button btUpload;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.Button txtClear;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ SignatureValidationApplication/SignatureValidationApplication/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SignatureValidationApplication
+{
+
+    public partial class Form1 : Form
+    {      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btBrowseResult_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "|*.csv";
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                //Display the file name and location
+                tbresult.Text = openFileDialog.FileName;
+            }
+        }
+        private void btUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dgCSVData.DataSource = GetDataTableFromCSVFile(tbresult.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Import CSV File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static DataTable GetDataTableFromCSVFile(string csvfilePath)
+        {
+            DataTable csvData = new DataTable();
+            DataTable dataTable = new DataTable();
+            StreamReader streamReader = new StreamReader(csvfilePath);
+            string[] totalData = new string[File.ReadAllLines(csvfilePath).Length];
+            totalData = streamReader.ReadLine().Split(',');
+            foreach (string column in totalData)
+            {
+                DataColumn datecolumn = new DataColumn(column);
+                datecolumn.AllowDBNull = true;
+                csvData.Columns.Add(datecolumn);
+            }
+            while (!streamReader.EndOfStream)
+            {
+                totalData = streamReader.ReadLine().Split(',');
+                csvData.Rows.Add(totalData);
+            }
+            return csvData;
+        }
+
+        private void txtClear_Click(object sender, EventArgs e)
+        {
+            dgCSVData.DataSource = null;
+            dgRepository.DataSource = null;
+        }
+
+        private void btnRepository_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "|*.csv";
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                //Display the file name and location
+                tbRepository.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            DataTable dtResult = GetDataTableFromCSVFile(tbresult.Text);
+            DataTable dtRepository = GetDataTableFromCSVFile(tbRepository.Text);
+            DataTable finalResult = new DataTable();
+            finalResult.Columns.Add("FileFolderName");
+            finalResult.Columns.Add("SignatureType");
+            finalResult.Columns.Add("Signature");
+            finalResult.Columns.Add("Result");
+            //Leading / Trailing quotation marks should be accounted for in all values 
+            const string quote = "\"";
+            //	Files and Folder names are case sensitive
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            for (int i = 0; i < dtResult.Rows.Count; i++)
+            {
+                string res = getResults(dtRepository, dtResult.Rows[i]["FileFolderName"].ToString(), dtResult.Rows[i]["SignatureType"].ToString(), dtResult.Rows[i]["Signature"].ToString());
+                //Leading / Trailing whitespaces should be removed from all values 
+                string filefolder = dtResult.Rows[i]["FileFolderName"].ToString().Trim();                
+                //Hash values should not contain any white spaces
+                string sig = dtResult.Rows[i]["Signature"].ToString().Trim();
+                //	Hash values should always be compared in UPPER case
+                string signature = sig.Trim().ToUpper();
+                string signaturetype = dtResult.Rows[i]["SignatureType"].ToString().Trim();
+                finalResult.Rows.Add(quote + filefolder + quote, quote + signaturetype + quote, quote + signature + quote, quote + res + quote);
+            }
+            dgRepository.DataSource = finalResult;
+        }
+        
+        private string getResults(DataTable dtRepository, string rowResultFile, string signatureType, string signature)
+        {
+            for (int j = 0; j < dtRepository.Rows.Count; j++)
+            {
+                if (rowResultFile.Contains(dtRepository.Rows[j]["imageName"].ToString()))
+                {
+                    switch (signatureType.ToString())
+                    {
+                        case "SHA1":
+                            {
+                                return getResult(signatureType, signature, dtRepository.Rows[j]["sigtype1"].ToString(), dtRepository.Rows[j]["sig1"].ToString());
+                            }
+                        case "MD5":
+                            {
+                                return getResult(signatureType, signature, dtRepository.Rows[j]["sigtype2"].ToString(), dtRepository.Rows[j]["sig2"].ToString());
+                            }
+                        case "CRC16":
+                            {
+                                return getResult(signatureType, signature, dtRepository.Rows[j]["sigtype3"].ToString(), dtRepository.Rows[j]["sig3"].ToString());
+                            }
+                        case "CRC32":
+                            {
+                                return getResult(signatureType, signature, dtRepository.Rows[j]["sigtype4"].ToString(), dtRepository.Rows[j]["sig4"].ToString());
+                            }
+                        case "HMACSHA1":
+                            {
+                                return getResult(signatureType, signature, dtRepository.Rows[j]["sigtype5"].ToString(), dtRepository.Rows[j]["sig5"].ToString());
+                            }
+                         default:
+                            return "Not Match";
+                    }
+                }
+            }
+            return "No File";
+        }
+
+        private void txtExport_Click(object sender, EventArgs e)
+        {
+            //Build the CSV file data as a Comma separated string.
+            string csv = string.Empty;
+            //Add the Header row for CSV file.
+            foreach (DataGridViewColumn column in dgRepository.Columns)
+            {
+                csv += column.HeaderText + ',';
+            }
+            //Add new line.
+            csv += "\r\n";
+            //Adding the Rows
+            foreach (DataGridViewRow row in dgRepository.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    //Add the Data rows.
+                    csv += cell.Value.ToString().Replace(",", ";") + ',';
+                }
+                //Add new line.
+                csv += "\r\n";
+            }
+            //Exporting to CSV.
+            string folderPath = "D:\\";
+            File.WriteAllText(folderPath + "DataGridViewExport.csv", csv);
+            label3.Text = "File 'DataGridViewExport' Exported Successfully in D Drive ";
+        }
+
+        private string getResult(string signatureTypeRes, string signatureRes, string signatureType, string signature)
+        {
+            if (signatureTypeRes.ToString() == signatureType)
+            {
+                if (signatureRes.ToString() == signature)
+                {
+                    return "Match";
+                }
+                else
+                {
+                    return "Not Match";
+
+                }
+            }
+            else
+            {
+                return "Not Match";
+            }
+        }
+    }
+}
+             
